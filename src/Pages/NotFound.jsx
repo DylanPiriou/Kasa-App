4cdf2d1e33db94eea587c54a7687e524a119d991
DylanPiriou <dylan.piriou@hotmail.com>
2023-02-03 15:07:42 +0100

    404 page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
+import "../Assets/styles/NotFound.css"
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="notfound">
+      <Navbar/>
+      <div className="notfound-content">
+        <span className='error-number'>404</span>
+        <h2 className='error-message'>Oups! La page que vous demandez n'existe pas.</h2>
+      </div>
+      <Link to="/" className="goback-home">Retourner sur la page d'accueil</Link>
+    </div>
   )
 }
