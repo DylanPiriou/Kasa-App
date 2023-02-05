fc5b459bb75a2a34e9d49bd936bb1f3ddb8d2922
DylanPiriou <dylan.piriou@hotmail.com>
2023-02-05 21:12:55 +0100

    slider logic

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Card({ key, item }) {
   return (
     <Link to={"/logement/" + item.id} state={{ item }} className="card" id={key}>
-        <img src={item.cover} alt="Logmement" />
+        <img src={item.cover} alt={item.title} />
         <h2 className='card-title'>{item.title}</h2>
     </Link>
   )
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Assets/styles/Slider.css"
 
 export default function Slider({ data }) {
+    const [currentImg, setCurrentImg] = useState(0);
+
+    const handlePrev = () => {
+        currentImg === 0 ? setCurrentImg(data.pictures.length - 1) : setCurrentImg(currentImg - 1)
+    }
+    const handleNext = () => {
+        currentImg === data.pictures.length - 1 ? setCurrentImg(0) : setCurrentImg(currentImg + 1)
+    }
+
   return (
     <div className="slider">
-        <img src={data.pictures[0]} className="slider-img" alt="Logement"/>
-        <i className="fa-solid fa-chevron-left"></i>
-        <i className="fa-solid fa-chevron-right"></i>
-        <span className='slider-img-number'>N°img/total</span>
+        <img src={data.pictures[currentImg]} className="slider-img" alt={data.title}/>
+        <i className="fa-solid fa-chevron-left" onClick={() => handlePrev()}></i>
+        <i className="fa-solid fa-chevron-right" onClick={() => handleNext()}></i>
+        <span className='slider-img-number'>{currentImg + 1}/{data.pictures.length}</span>
     </div>
   )
 }
