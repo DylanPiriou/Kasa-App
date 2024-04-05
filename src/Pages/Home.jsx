258863bf733534734029cd2bb4ba09cdaefcb325
DylanPiriou <dylan.piriou@hotmail.com>
2024-04-05 15:45:44 +0200

    feat: absolute path

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Banner from '../Components/Banner'
-import Navbar from '../Components/Navbar'
-import BackgroundHome from '../Assets/imgs/home-bg.jpg'
-import GridCards from '../Components/GridCards'
-import Footer from '../Components/Footer'
+import React from "react";
+import Banner from "../Components/Banner";
+import Navbar from "../Components/Navbar";
+import BackgroundHome from "../Assets/imgs/home-bg.jpg";
+import GridCards from "../Components/GridCards";
+import Footer from "../Components/Footer";
 
 export default function Home() {
-  const title = "Chez vous, partout et ailleurs"
-  return (
-    <div className="home">
-      <Navbar/>
-      <Banner bg={BackgroundHome} title={title}/>
-      <GridCards/>
-      <Footer/>
-    </div>
-  )
+	const title = "Chez vous, partout et ailleurs";
+	return (
+		<div className="home">
+			<Navbar />
+			<Banner bg={BackgroundHome} title={title} />
+			<GridCards />
+			<Footer />
+		</div>
+	);
 }
