1dc770e2050caf7b2642f044023da9f99b57912a
DylanPiriou <dylan.piriou@hotmail.com>
2024-04-05 15:54:59 +0200

    fix: absolute path

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import '../Assets/styles/Banner.scss'
+import React from "react";
+import "../Assets/styles/Banner.scss";
 
 export default function Banner({ bg, title }) {
-  return (
-    <div className="banner">
-        <img src={`.${bg}`} className='banner-img' alt="nature" />
-        <h1 className='banner-title'>{title}</h1>
-    </div>
-  )
+	return (
+		<div className="banner">
+			<img src={`.${bg}`} className="banner-img" alt="nature" />
+			<h1 className="banner-title">{title}</h1>
+		</div>
+	);
 }
