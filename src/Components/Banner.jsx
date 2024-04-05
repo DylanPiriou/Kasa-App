ef33aa4af8661e3cd8a8ae64d88db64356c6ee95
DylanPiriou <dylan.piriou@hotmail.com>
2024-04-05 17:30:04 +0200

    feat: test deploy

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import "../Assets/styles/Banner.scss";
 
 export default function Banner({ bg, title }) {
-	const absoluteUrl = window.location.origin + bg;
-
 	return (
 		<div className="banner">
-			<img src={absoluteUrl} className="banner-img" alt="nature" />
+			<img src={bg} className="banner-img" alt="nature" />
 			<h1 className="banner-title">{title}</h1>
 		</div>
 	);
