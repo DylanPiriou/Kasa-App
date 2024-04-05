25dcb3903baf2293d5ee673d49d1da63284b8ea0
DylanPiriou <dylan.piriou@hotmail.com>
2024-04-05 16:14:09 +0200

    fix: absolute patch test

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "../Assets/styles/Banner.scss";
 
 export default function Banner({ bg, title }) {
+	const absoluteUrl = window.location.origin + bg;
+
 	return (
 		<div className="banner">
-			<img src={bg} className="banner-img" alt="nature" />
+			<img src={absoluteUrl} className="banner-img" alt="nature" />
 			<h1 className="banner-title">{title}</h1>
 		</div>
 	);
