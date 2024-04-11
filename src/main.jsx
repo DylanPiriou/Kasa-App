32a74f260bee41a55c8e44aa901f81a999246622
DylanPiriou <dylan.piriou@hotmail.com>
2024-04-12 01:44:08 +0200

    feat: loading page addded

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./Assets/styles/index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Loading from "./Components/Loading";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<Loading />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
