06af8dbfc6dcc56645d09b21a524487b09fccfeb
DylanPiriou <dylan.piriou@hotmail.com>
2024-04-12 23:28:51 +0200

    style: collapse transition

--- src/Components/Collapse.jsx
+++ src/Components/Collapse.jsx
@@ -1,24 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../Assets/styles/Collapse.scss";
 
 export default function Collapse({ title, content, index }) {
 	const [isActive, setIsActive] = useState(false);
 
+	const collapseRef = useRef(null);
+	const contentRef = useRef(null);
+
+	const [elHeight, setElHeight] = useState("");
+	useEffect(() => {
+		setElHeight(contentRef.current.offsetHeight);
+	}, [])
+
 	const handleCollapse = () => {
-		setIsActive((current) => !current);
+		if (isActive) {
+			contentRef.current.style.transform = "translateY(-100%)";
+			collapseRef.current.style.height = "48px";
+			setIsActive(false);
+		} else {
+			contentRef.current.style.transform = "translateY(0)";
+			collapseRef.current.style.height = `${elHeight + 47}px`;
+			setIsActive(true);
+		}
 	};
 
 	return (
-		<div className="collapse">
-			<button className="collapse-btn" onClick={() => handleCollapse()}>
-				<h2 className="collapse-title">{title}</h2>
-				{isActive ? (
-					<i className="fa-solid fa-chevron-up"></i>
-				) : (
-					<i className="fa-solid fa-chevron-down"></i>
-				)}
-			</button>
-			{isActive && <div className="collapse-content">{content}</div>}
-		</div>
+			<div className="collapse" ref={collapseRef}>
+				<button className="collapse-btn" onClick={() => handleCollapse()}>
+					<h2 className="collapse-title">{title}</h2>
+					{isActive ? (
+						<i className="fa-solid fa-chevron-up"></i>
+					) : (
+						<i className="fa-solid fa-chevron-down"></i>
+					)}
+				</button>
+				<div className="collapse-content" ref={contentRef}>
+					{content}
+				</div>
+			</div>
 	);
 }
